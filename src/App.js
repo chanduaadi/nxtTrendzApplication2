df34f4337ade8b3e55c8d26469848a4171c8db31
chanduaadi <chanduaadi2002@gmail.com>
2023-01-21 12:42:13 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/products" component={Products} />
-      <Route exact path="/cart" component={Cart} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/products" component={Products} />
+    <Route exact path="/cart" component={Cart} />
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App
